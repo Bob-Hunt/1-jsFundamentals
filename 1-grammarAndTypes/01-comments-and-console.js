f65926bbd64f6fbd00c87418ec1a7065d0086099
BobHunt <bomazani@icloud.com>
2021-06-16 07:03:45 -0400

    Initial commit. Completed 'comments-and-console.js' video.

---
+++ 1-grammarAndTypes/01-comments-and-console.js
@@ -0,0 +1,42 @@
+/*
+COMMENTS
+
+1-grammarAndTypes
+    01-comments-and-consoles.js
+*/
+
+// value of a comment:
+// communicates to anyone reading our code
+
+// types of comments
+// 1) single-line comment
+
+/*
+2)
+multi-line
+comment
+*/
+
+// console.log()
+// ouputs some message to the console
+console.log('This is a console.log message!');
+console.log("You can separate parts of a console message with commas,","just like this.");
+
+// console logging and debugging:
+// We can debug with console.log in two big ways:
+// 1) We can check the status of some JS 'thing'
+// 2) We can check a process we have built with repeated console.log messages
+
+// Example 1)
+let sampleName = 'Bob';
+console.log(sampleName);
+
+// Example 2)
+let sampleNum = 1;
+console.log(sampleNum);
+sampleNum++
+console.log(sampleNum);
+sampleNum++
+console.log(sampleNum);
+
+
